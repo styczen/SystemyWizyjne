e7499760f3578645e7a1c82aa5c8b94cd8f0c8cc
Bartłomiej Styczeń <daltar96@gmail.com>
2017-04-15 23:41:26 +0200

    Podstawowe dzialanie

--- lab7/lab7_1.m
+++ lab7/lab7_1.m
@@ -3,34 +3,38 @@ clear all, close all;
 
 obraz = imread('knee.png');
 obraz = double(obraz);
+[img_x, img_y] = size(obraz);
+
 figure(1)
 imshow(obraz, [])
-stos = zeros(10000, 2);
-visited = uint8(zeros(779, 693));
-segmented = uint8(zeros(779, 693));
-[x, y] = ginput(1);
-% x = 347; y = 287; % punkt startowy (zaokraglenie w dol)
-x = floor(x); y = floor(y);
-stos(1,1) = x; stos(1,2) = y;
-visited(x,y) = 1; segmented(x,y) = 1;
-threshold = 4;
-% (j == stos(i,1)) && (k == stos(i,2)) || 
-% petla
-for i = 1:size(stos, 1)
-   temp_x = stos(i,1); temp_y = stos(i,2);
-   for  j = (stos(i,1)-1):1:(stos(i,1)+1)
-      for k = (stos(i,2)-1):1:(stos(i,2)+1)
-          if (j < 1) || (k < 1)
-              continue
-          end
-          if visited(k,j) == 0
-             if abs(obraz(k,j) - obraz(x,y)) < 100
-                segmented(k,j) = 1; 
-                stos(i+1, 1) = j; stos(i+1,2) = k;
-             end
-             visited(k,j) = 1;
-          end
 
+stos = zeros(10000, 2);
+visited = zeros(img_x, img_y);
+segmented = zeros(img_x, img_y);
+
+[y_set, x_set] = ginput(1); % punkt setowy
+x_set = floor(x_set); y_set = floor(y_set); % zaokraglenie do calkowitej wartosci
+
+stos(1,1) = x_set; stos(1,2) = y_set;
+i = 1; % wskaznik na szczyt stosu
+
+visited(x_set,y_set) = 1; segmented(x_set,y_set) = 1;
+threshold = 40;
+
+% petla
+while i ~= 0
+   temp_x = stos(i,1); temp_y = stos(i,2);
+   i = i - 1; % sciagniecie ze stosu
+   for  j = (temp_x-1):(temp_x+1)
+      for k = (temp_y-1):(temp_y+1)
+          if visited(j,k) == 0
+             if abs(obraz(j,k) - obraz(x_set,y_set)) < threshold
+                segmented(j,k) = 1; 
+                i = i + 1;
+                stos(i, 1) = j; stos(i,2) = k;
+             end
+             visited(j,k) = 1;
+          end
       end
    end
 end
