d0b033b9ef9db927534b25f6a9ca0f1b3ec6d499
Bartłomiej Styczeń <daltar96@gmail.com>
2017-05-08 19:08:07 +0200

    Dokonczenie labow 8

--- lab8/lab8_2.m
+++ lab8/lab8_2.m
@@ -1,4 +1,4 @@
-% II. Rozpoznawanie obiektow za pomocą wspolczynników ksztaltu
+% II. Rozpoznawanie obiektow za pomoca wspolczynnika ksztaltu
 clear all; close all
 
 % 1. Indeksacja wbudowana w IPT Matlab'a
@@ -21,25 +21,103 @@ title('8 sasiadow')
 figure(2)
 labeled = bwlabel(obraz, 8);
 imshow(labeled, [])
-% Ponumerowanie obiektow
+
+wsp = obliczWspolczynniki(labeled);
+[XX, YY] = size(labeled);
+
+figure(3)
+subplot(2, 3, 1)
+imshow(labeled, [])
+title('Oryginal')
 r = regionprops(labeled,'Centroid');
 for i=1:length(r)
     text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
 end
-wsp = obliczWspolczynniki(labeled);
 
-[XX, YY] = size(labeled);
-
+subplot(2, 3, 2)
+labeled = uint8(bwlabel(obraz, 8));
 for i = 1:XX
    for j = 1:YY
         piksel = labeled(i,j);
-        if piksel ~= 0 && ~(wsp(piksel,1) > 0.73 && wsp(piksel,1) < 0.86)
+        if piksel ~= 0 && ~(wsp(piksel,1) > 0.77 && wsp(piksel,1) < 0.79) % kolo 1 i 6
             labeled(i,j) = 0;
         end
    end
 end
-
-figure(3)
 imshow(labeled, [])
+title('Compactness')
+r = regionprops(labeled,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
+subplot(2, 3, 3)
+labeled = bwlabel(obraz, 8);
+for i = 1:XX
+   for j = 1:YY
+        piksel = labeled(i,j);
+        if piksel ~= 0 && ~(wsp(piksel,2) > 0.51 && wsp(piksel,2) < 0.53) % krzyzyk 2, 5, 8
+            labeled(i,j) = 0;
+        end
+   end
+end
+imshow(labeled, [])
+title('Rmin/Rmax')
+r = regionprops(labeled,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
+subplot(2, 3, 4)
+labeled = bwlabel(obraz, 8);
+for i = 1:XX
+   for j = 1:YY
+        piksel = labeled(i,j);
+        if piksel ~= 0 && ~(wsp(piksel,3) > 6.76 && wsp(piksel,3) < 6.82) % kwadrat 3, 4, 7
+            labeled(i,j) = 0;
+        end
+   end
+end
+imshow(labeled, [])
+title('Blair - Bliss')
+r = regionprops(labeled,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
+subplot(2, 3, 5)
+labeled = bwlabel(obraz, 8);
+for i = 1:XX
+   for j = 1:YY
+        piksel = labeled(i,j);
+        if piksel ~= 0 && ~(wsp(piksel,4) > 0.995 && wsp(piksel,4) < 1.1) % kolo 1, 6
+            labeled(i,j) = 0;
+        end
+   end
+end
+imshow(labeled, [])
+title('Haralick')
+r = regionprops(labeled,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
+subplot(2, 3, 6)
+labeled = bwlabel(obraz, 8);
+for i = 1:XX
+   for j = 1:YY
+        piksel = labeled(i,j);
+        if piksel ~= 0 && ~(wsp(piksel,5) > 0.01 && wsp(piksel,5) < 0.02) % krzyz 2, 5, 8
+            labeled(i,j) = 0;
+        end
+   end
+end
+imshow(labeled, [])
+title('M7')
+r = regionprops(labeled,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
 
 
---
+++ lab8/lab8_3.m
@@ -0,0 +1,55 @@
+% Indeksacja rzeczywistego obrazu
+% II. Rozpoznawanie obiektow za pomoc? wspolczynników ksztaltu
+% 2. Wspolczynniki ksztaltu
+% 6.
+clear all; close all;
+image = imread('ksztaltyReal.bmp');
+[XX, YY, ZZ] = size(image);
+
+figure(1)
+subplot(1, 3, 1)
+imshow(image, [])
+title('Oryginal')
+% image_bin = im2bw(image);
+image_bin = zeros(XX, YY);
+
+trigger = 50;
+
+for i=1:XX
+    for j=1:YY    
+        if image(i,j,1) < trigger && image(i,j,2) < trigger && image(i,j,3) < trigger
+            image_bin(i,j) = 1;
+        else
+            image_bin(i,j) = 0;
+        end
+    end
+end
+
+% Wyostrzenie krawedzi
+M = [1 2 1; 2 4 2; 1 2 1]; % macierz do wyostrzenia krawedzi
+M = M/sum(sum(M)); % normalizacja
+image_bin = conv2(image_bin, M, 'same');
+
+subplot(1, 3, 2)
+imshow(image_bin, [])
+title('Binaryzaja')
+
+filtered = uint8(bwlabel(image_bin,8));
+r = regionprops(filtered,'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
+end
+
+wsp = obliczWspolczynniki(filtered);
+for i = 1:XX
+   for j = 1:YY
+        piksel = filtered(i,j);
+        if piksel ~= 0 && ~(wsp(piksel,2) > 0.61 && wsp(piksel,2) < 0.66)
+            filtered(i,j) = 0;
+        end
+   end
+end
+
+subplot(1, 3, 3)
+imshow(filtered, [])
+title('Krzyze - Rmin/Rmax')
