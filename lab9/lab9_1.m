10aea0eaad0beb07805b8d8fb6e639f037667b6b
Bartłomiej Styczeń <daltar96@gmail.com>
2017-05-15 17:13:55 +0200

    Dziala - magia

--- lab9/lab9_1.m
+++ lab9/lab9_1.m
@@ -1,23 +1,54 @@
-% Segmentacja przez podział i scalanie
+% Segmentacja przez podzial i scalanie
+
 clear all; close all;
+
+global trigger minimum index segRes mean_table;
 
 rgb_image = imread('umbrealla.png');
 
-figure(1)
-imshow(rgb_image, []);
+figure
+imshow(rgb_image);
+title('Oryginal')
 
-hsv_image = rgb2hsv(rgb_image); % konwersja z RGB na HSV
+hsv_image = double(rgb2hsv(rgb_image));
+h_image = hsv_image(:,:,1);
 
-hsv_image_hpart = hsv_image(:,:,1);
-[X, Y] = size(hsv_image_hpart);
-global segRes trigger minimum index;
-segRes = ones(X, Y);
-index = 0;
-trigger = 0.05;
-minimum = 8;
+[X, Y] = size(h_image);
 
-set(0, 'RecursionLimit', 1000)
-split(hsv_image_hpart, 1, 1, X, Y);
-temp = ones(X, Y);
-imshow(hsv_image_hpart, [])
+mean_table = zeros(X, Y);
+index = 1;
+trigger_merge = 0.03;
+segRes = zeros(X, Y);
+trigger = 0.02;
+minimum = 4;
+split(h_image, 1, 1, X, Y);
 
+figure
+imshow(mean_table, []);
+title('Mean table')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i_index = 1:index
+    
+    IB = segRes == i_index;
+    IBDilate = imdilate(IB,strel('square',3));
+    IBDiff = IBDilate - IB;
+    IBMult = IBDiff.*segRes;
+    IBMultNZ = nonzeros(IBMult);
+    IBUnique = unique(IBMultNZ);
+    
+    for i = 1:length(IBUnique)
+        IBS = segRes == IBUnique(i);
+        [y_IBS , x_IBS] = find(IBS, 1 , 'first');
+        [y_IB, x_IB] = find(IB, 1, 'first');
+        if abs(mean_table(y_IBS, x_IBS) - mean_table(y_IB, x_IB)) < trigger_merge
+            segRes(IBS) = i_index;
+        end
+    end
+end
+
+figure
+imshow(label2rgb(segRes), []);
+% imshow(segRes, []);
+title('Segmented')
--- lab9/split.m
+++ lab9/split.m
@@ -1,19 +1,24 @@
-function [ mean_, std_ ] = split(image, x1, y1, x2, y2)
-global segRes trigger minimum index;
+function [] = split(image, x1, y1, x2, y2)
+
+global segRes trigger minimum index mean_table
 
 temp = image(x1:x2, y1:y2);
-mean_ = mean(temp(:));
-std_ = std(temp(:));
-[row, col] = size(temp);
+% area_v=double(temp(:));
+[row, col]=size(temp);
+mean_ = mean(double(temp(:)));
+std_ = std(double(temp(:)));
 
-if std_ > trigger && row > minimum && col > minimum
-    split(image, x1, y1, round(x2/2), round(y2/2));
-    split(image, x1, round(y2/2), round(x2/2), y2);
-    split(image, round(x2/2), y1, x2, round(y2/2));
-    split(image, round(x2/2), round(y2/2), x2, y2);
-else
-    index = index + 1;
-    segReg(y1:y2, x1:x2) = index;
-end
+    if std_ > trigger && row > minimum && col > minimum
+%         x_prim=(round((x2-x1)/2)+x1);
+%         y_prim=(round((y2-y1)/2)+y1);
+        split(image, x1, y1, round((x2-x1)/2)+x1, round((y2-y1)/2)+y1);
+        split(image, x1, round((y2-y1)/2)+y1, round((x2-x1)/2)+x1, y2);
+        split(image, round((x2-x1)/2)+x1, y1, x2, round((y2-y1)/2)+y1);
+        split(image, round((x2-x1)/2)+x1, round((y2-y1)/2)+y1, x2, y2);
+    else 
+        index = index + 1;
+        segRes(x1:x2, y1:y2) = index;
+        mean_table(x1:x2, y1:y2) = mean_;
+    end
 
 end
