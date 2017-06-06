f53f5d93c5f7dd2de766df3188b0e32db5a3bb5c
Bartłomiej Styczeń <daltar96@gmail.com>
2017-06-06 16:46:15 +0200

    Punkt D zrobiony

--- lab13/lab13_D.m
+++ lab13/lab13_D.m
@@ -1,10 +1,40 @@
 % D. Metoda filtracji Wienera
 clear all; close all
 
+img = imread('wiener_motion_noisy.png');
 
+figure
+subplot(1, 4, 1)
+imshow(img, [])
+title('Oryginal')
 
+img = im2double(img);
+PSF = fspecial('motion', 15, 0);
 
+SNR = 70; % dB
 
+% NSR = 1/SNR
+NSR = 1/SNR;
+J = deconvwnr(img, PSF, NSR);
+subplot(1, 4, 2)
+imshow(J, []);
+title('1/SNR')
+
+% NSR = wariancja_szumu / wariancja_obrazu
+wariancja_szumu = 1.0046e-005;
+wariancja_obrazu = var(img(:));
+NSR = wariancja_szumu / wariancja_obrazu;
+J = deconvwnr(img, PSF, NSR);
+subplot(1, 4, 3)
+imshow(J, []);
+title('wariancja szumu / wariancja obrazu')
+
+% NSR = 2*wariancja_obrazu
+NSR = 2*wariancja_obrazu;
+J = deconvwnr(img, PSF, NSR);
+subplot(1, 4, 4)
+imshow(J, []);
+title('2*wariancja obrazu')
 
 
 
